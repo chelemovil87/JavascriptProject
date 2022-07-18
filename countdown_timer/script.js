b1fc9483fbee675d4e27ae2f532c8578be974c42
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2022-07-18 06:09:37 +0200

    .

--- countdown_timer/script.js
+++ countdown_timer/script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const vacations = ();
+const vacations = ("21 Dec 2022");
 function countdown() {
   const vacationsDate = new Date(vacations);
   const currenDate = new Date();
