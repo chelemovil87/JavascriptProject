98f3987b3a757d06986d3f9381f420bf4f91858a
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2022-07-18 06:52:39 +0200

    /

---
+++ quiz-app/script.js
@@ -0,0 +1,18 @@
+//crear el cuestionario 
+const quizData = [
+    {
+        question: "quien gano el mundial del 2010",
+        a: "Alemania",
+        b: "Suecia",
+        c: "España",
+        d: "Brasil",
+        correct: "c"
+    }, {
+        question: "De que color es el cielo",
+        a: "azul",
+        b: "verde",
+        c: "rojo",
+        d: "naranja",
+        correct:"a"
+    }
+]
